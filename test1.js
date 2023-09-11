428a1fa4ffd5bcd0304461e2c3544528d781f42b
stefandobrovolskiy <144222943+stefandobrovolskiy@users.noreply.github.com>
2023-09-11 15:48:12 +0300

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,17 @@
+let arr = []
+
+arr [0] = 1
+arr [1] = "UKD"
+arr [2] = NaN
+arr [3] = true
+arr [4] = "undefiened"
+
+let me = {
+    name: "Stefan",
+    surname: "Dobrovolskiy",
+    age: 17,
+    home: "Ivano-Frankivsk"
+}
+
+console.table(arr)
+console.table(me)
